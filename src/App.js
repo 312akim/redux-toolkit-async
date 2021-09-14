9427d6a7ae461c48b0c36427ccc6701e91c560f2
Alex Kim <312akim@gmail.com>
2021-09-13 18:23:21 -0700

    Feature: Async updating of firebase data status, added notification bar to display data status

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.isOpen);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification)
+
+  useEffect(() => {
+    // Won't replace cart data on first load
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    
+    // Using Action Creators for async functions - Keeps components clean & logic in slices
+    // Replace cart data on cart state change
+    dispatch(sendCartData(cart));
+
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {
+        notification && 
+          <Notification 
+            status={notification.status} 
+            title={notification.title} 
+            message={notification.message}
+          />
+      }
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isOpen: true
+    isOpen: true,
+    notification: null
 }
 
 const uiSlice = createSlice({
@@ -11,6 +12,13 @@ const uiSlice = createSlice({
         toggle(state) {
             state.isOpen = !state.isOpen;
         },
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status, 
+                title: action.payload.title,
+                message: action.payload.message
+            };
+        }
     }
 });
 
